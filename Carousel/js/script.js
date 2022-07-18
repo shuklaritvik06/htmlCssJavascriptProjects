87f4c62f03788044cfdbca201c48db1f1c4039d2
Ritvik Shukla <ritvikshukla261@gmail.com>
2022-07-18 22:19:23 +0530

    Add autostart

--- Carousel/js/script.js
+++ Carousel/js/script.js
@@ -7,12 +7,14 @@ let curSlide = 0;
 const nextSlideBtn = document.querySelector(".btn-next");
 const prevSlideBtn = document.querySelector(".btn-prev");
 
-nextSlideBtn.addEventListener("click", function () {
+const nextSlide =  function () {
   curSlide===slides.length-1?curSlide=0:curSlide++;  
   slides.forEach((slide, indx) => {
     slide.style.transform = `translateX(${100 * (indx - curSlide)}%)`;
   });
-});
+}
+
+nextSlideBtn.addEventListener("click",nextSlide);
 
 prevSlideBtn.addEventListener("click", () => {
   curSlide===0 ? curSlide = slides.length - 1 : curSlide--;
@@ -20,3 +22,7 @@ prevSlideBtn.addEventListener("click", () => {
     slide.style.transform = `translateX(${100 * (indx - curSlide)}%)`;
   });
 });
+
+setInterval(()=>{
+   nextSlide()
+},1000);
